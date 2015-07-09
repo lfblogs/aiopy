d994e3bbd69ff81d070483e18aa5c2dd9ef70eea
Liu Fei <13701242710@163.com>
2015-07-09 13:46:18 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from distutils.core import setup
 setup(
     name='Pyaio',
     version='1.0.2',
-    packages=['build.lib.Pyaio', 'build.lib.Pyaio.db', 'build.lib.Pyaio.orm', 'build.lib.Pyaio.apis',
-              'build.lib.Pyaio.conf', 'build.lib.Pyaio.http', 'build.lib.Pyaio.utils', 'Pyaio', 'Pyaio.db', 'Pyaio.app',
+    packages=[ 'Pyaio', 'Pyaio.db', 'Pyaio.app',
               'Pyaio.orm', 'Pyaio.apis', 'Pyaio.conf', 'Pyaio.http', 'Pyaio.utils'],
     url='http://github.com/lfblogs/Pyaio',
     license='GUN License',
