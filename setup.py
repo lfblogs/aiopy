9ae453e95397aee70aa0d9a5a8fb7fe18b5df349
Liu Fei <13701242710@163.com>
2015-07-09 10:07:39 +0800

    1.0.1
    update init files

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name='Pyaio',
     version='1.0.1',
-    packages=['', 'db', 'orm', 'apis', 'conf', 'http', 'utils'],
-    url='http://github.com/lfblogs/Pyaio',
+    packages=['Pyaio', 'Pyaio.db', 'Pyaio.orm', 'Pyaio.apis', 'Pyaio.conf', 'Pyaio.http', 'Pyaio.utils'],
+    url='http://github,com/lfblogs/Pyaio',
     license='GUN License',
-    author='lf',
+    author='lfblogs',
     author_email='13701242710@163.com',
     description='Python Simple Web Frame'
 )
