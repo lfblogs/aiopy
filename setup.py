10e9efeedeb7f4d65ba245012315b034b88efc05
Liu Fei <13701242710@163.com>
2015-07-20 15:27:58 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='aiopy',
-    version='1.1.0',
+    version='1.1.6',
     packages=['aiopy', 'aiopy.db', 'aiopy.api', 'aiopy.app', 'aiopy.conf', 'aiopy.http', 'aiopy.utils',
               'aiopy.required', 'aiopy.required.aiohttp', 'aiopy.required.aiomysql', 'aiopy.required.aiomysql.sa'],
     url='http://github.com/lfblogs',
